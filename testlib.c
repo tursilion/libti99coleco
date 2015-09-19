6ad6aa6cdbccb8c080c9bde4978691fa715ebd55
tursi <tursi@harmlesslion.com>
2015-09-19 15:43:16 +0100

    -test app updates -race improvement in vdpwaitint

--- testlib.c
+++ testlib.c
@@ -2,10 +2,10 @@
 #include "halt.h"
 
 // tested
-
 int main() {
 	unsigned char x = set_text();
 	unsigned char col = (COLOR_GRAY<<4)|COLOR_DKBLUE;
+	unsigned char cnt = 0;
 	// TODO: test - do we need delay between register writes?
 	VDP_SAFE_DELAY();
 	VDP_SET_REGISTER(VDP_REG_COL, col);
@@ -21,8 +21,17 @@ int main() {
 			++col;
 			VDP_SET_REGISTER(VDP_REG_COL, col);
 		}
-		VDP_SET_ADDRESS_WRITE(14);	// top center
+		if (vdpwaitvint()) {	// wait for an int to occur
+			// it was late - change colors - should not see this happen
+			++col;
+			VDP_SET_REGISTER(VDP_REG_COL, col);
+		}
+
+		VDP_SET_ADDRESS_WRITE(14);	// position for counter
 		faster_hexprint(VDP_INT_COUNTER);
+
+		VDP_SET_ADDRESS_WRITE(54);	// position for counter
+		faster_hexprint(++cnt);
 	}
 
 	return 0;
--- vdp_waitvint.c
+++ vdp_waitvint.c
@@ -4,14 +4,30 @@ unsigned char gSaveIntCnt;	// console interrupt count byte
 
 // returns non-zero if interrupt already fired (ie: we are late)
 unsigned char vdpwaitvint() {
-	unsigned char ret = vdpLimi & 0x80;
+	unsigned char ret = 0;
 
 	// wait for a vertical interrupt to occur (enables interrupts - first call may not wait)
-	VDP_INT_ENABLE; 
+	// to avoid a race on the return condition, we reproduce VDP_INT_ENABLE here
+
+	// set the enable flag
+	__asm__("\tpush hl\n\tld hl,#_vdpLimi\n\tset 0,(hl)\n\tpop hl"); 
+	
+	// check if we missed one despite that
+	if (vdpLimi&0x80) {
+		my_nmi(); 
+		ret = 1;
+	}
+	
+	// wait for the interrupt to run (if we missed it, it ran already and this won't wait)
 	while (VDP_INT_COUNTER == gSaveIntCnt) { } 
-	gSaveIntCnt=VDP_INT_COUNTER; 
+
+	// turn the interrupt flag back off
 	VDP_INT_DISABLE; 
 
+	// remember the new value
+	gSaveIntCnt=VDP_INT_COUNTER; 
+
+	// back to caller
 	return ret;
 }
 
